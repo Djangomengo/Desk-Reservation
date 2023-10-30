b697a28239db36ee21e0c0d5ae166d69abd2ec65
Djangomengo <87585829+Djangomengo@users.noreply.github.com>
2023-10-30 15:01:22 +0100

    hier
    ab hier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {ConfigModule, ConfigService} from "@nestjs/config";
-import {config} from "dotenv";
 import {getDatabaseConfig} from "./shared/database/database.config";
 
 @Module({
